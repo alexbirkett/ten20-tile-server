c8035e3a596e347754ed61e8923946cd8c4ce6c9
Alex Birkett <alex@birkett.co.uk>
2014-02-12 21:14:04 +0100

    Log exceptions

--- app.js
+++ app.js
@@ -12,36 +12,40 @@ var argv = require('optimist')
     .argv;
 
 tilelive.load(argv.url, function(err, source) {
-    if (err) {
-        throw err;
-    }
 
-    // all environments
-    app.set('port', process.env.PORT || 7777);
-    app.use(express.logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(express.methodOverride());
-    app.use(app.router);
+    try {
+        // all environments
+        if (err) {
+            throw err;
+        }
+        app.set('port', process.env.PORT || 7777);
+        app.use(express.logger('dev'));
+        app.use(express.json());
+        app.use(express.urlencoded());
+        app.use(express.methodOverride());
+        app.use(app.router);
 
-    if ('development' == app.get('env')) {
-        app.use(express.errorHandler());
-    }
+        if ('development' == app.get('env')) {
+            app.use(express.errorHandler());
+        }
 
-    app.get('/', routes.index);
-    app.get(/^\/v2\/tiles\/(\d+)\/(\d+)\/(\d+).png$/, function(req, res){
+        app.get(/^\/v2\/tiles\/(\d+)\/(\d+)\/(\d+).png$/, function(req, res){
 
-        var z = req.params[0];
-        var x = req.params[1];
-        var y = req.params[2];
+            var z = req.params[0];
+            var x = req.params[1];
+            var y = req.params[2];
 
-        source.getTile(z, x, y, function(err, tile, headers) {
-            res.send(tile);
+            source.getTile(z, x, y, function(err, tile, headers) {
+                res.send(tile);
+            });
         });
-    });
 
-    http.createServer(app).listen(app.get('port'), function(){
-        console.log('Express server listening on port ' + app.get('port'));
-    });
+        http.createServer(app).listen(app.get('port'), function() {
+            console.log('Express server listening on port ' + app.get('port'));
+        });
+    } catch(e) {
+        console.log(e);
+        throw e;
+    }
 
 });
